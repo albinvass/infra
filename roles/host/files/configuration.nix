0a79727bebfbf1ad0c502f963f04a914a2428033
Albin Vass <albin.vass@gmail.com>
2022-11-26 14:16:26 +0100

    Cleanup

--- roles/host/files/configuration.nix
+++ roles/host/files/configuration.nix
@@ -13,20 +13,7 @@
       ./hardware-configuration.nix # Include the results of the hardware scan.
       ./config/role.nix
       ./config/nginx.nix
-      #./config/zuul
-      #./config/zookeeper
-      #./config/mysql
   ]; age.secrets.vinescore-oauth-token.file = ./secrets/vinescore-oauth-token.age;
-  #virtualisation.oci-containers.containers = {
-  #  vinemetrics-irc = {
-  #    image = "vinemetrics-irc:${lib.head (lib.strings.splitString "-" (baseNameOf vinemetrics-irc.packages.x86_64-linux.container))}";
-  #    imageFile = vinemetrics-irc.packages.x86_64-linux.container;
-  #    environmentFiles = [ config.age.secrets.vinescore-oauth-token.path ];
-  #    environment = {
-  #      VINESCORE_CHANNELS = "#vassast";
-  #    };
-  #  };
-  #};
 
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
@@ -51,9 +38,6 @@
     8080
     8443  # synapse
     8448  # synapse
-    9000  # zuul
-    9090  # prometheus
-    3000  # grafana
   ];
   networking.interfaces.bridge0.useDHCP = true;
   networking.bridges = {

04a9b1732dfa8bd0cda7cb50cb8e86cea0d76806
Albin Vass <albin.vass@gmail.com>
2023-11-13 11:12:27 +0100

    Fix minio

--- nixos/hosts/devbox/default.nix
+++ nixos/hosts/devbox/default.nix
@@ -52,7 +52,7 @@
   fileSystems."/var/lib/davfs2" = {
       device = "https://u343362.your-storagebox.de";
       fsType = "davfs";
-      options = ["rw,file_mode=0660,dir_mode=0700" "0" "0"];
+      options = ["rw,file_mode=0660,dir_mode=0755" "0" "0"];
   };
 
   services.cloudflared = {
--- nixos/hosts/devbox/minio/default.nix
+++ nixos/hosts/devbox/minio/default.nix
@@ -8,8 +8,8 @@ in rec {
       "minio-root-credentials" = {
         destDir = "/etc/minio";
         keyCommand = ["bws-get" "minio-root-credentials"];
-        user = "root";
-        group = "root";
+        user = "minio";
+        group = "minio";
         permissions = "0600";
       };
     };
@@ -20,25 +20,14 @@ in rec {
     "minio.albinvass.se" = "http://${consoleAddress}";
   };
 
-  containers.minio = {
-    autoStart = true;
-    bindMounts = {
-      "/etc/minio" = {
-        hostPath = "/etc/minio";
-        isReadOnly = true;
-      };
-      "/var/lib/minio" = {
-        hostPath = "/var/lib/davfs2/minio";
-        isReadOnly = false;
-      };
-    };
-    config = { config, pkgs, lib, ...}: {
-      services.minio = {
-        enable = true;
-        region = "eu-north-1";
-        listenAddress = listenAddress;
-        consoleAddress = consoleAddress;
-      };
-    };
+  environment.systemPackages = with pkgs; [ minio-client ];
+  services.minio = {
+    enable = true;
+    region = "eu-north-1";
+    rootCredentialsFile = "/etc/minio/minio-root-credentials";
+    listenAddress = listenAddress;
+    consoleAddress = consoleAddress;
+    dataDir = ["/var/lib/davfs2/minio/data"];
+    configDir = "/var/lib/davfs2/minio/config";
   };
 }

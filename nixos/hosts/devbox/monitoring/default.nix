3dfa583243fd8157c7286b38396187a23f7c6874
Albin Vass <albin.vass@gmail.com>
2023-11-18 20:40:09 +0100

    add kibana

--- nixos/hosts/devbox/monitoring/default.nix
+++ nixos/hosts/devbox/monitoring/default.nix
@@ -1,21 +1,27 @@
 { config, ... }: {
-  services.cloudflared.tunnels.devbox.ingress = {
-    "prometheus.albinvass.se" = "http://localhost:${builtins.toString config.services.prometheus.port}";
-    "elastic.albinvass.se" = "http://localhost:${builtins.toString config.services.elasticsearch.port}";
-    "grafana.albinvass.se" = "http://localhost:${builtins.toString config.services.grafana.settings.server.http_port}";
-  };
-  services.prometheus = {
-    enable = true;
-    stateDir = "prometheus";
-  };
-  services.elasticsearch = {
-    enable = true;
-  };
-  services.grafana = {
-    enable = true;
-    settings = {
-      server = {
-        root_url = "https://grafana.albinvass.se";
+  services = {
+    cloudflared.tunnels.devbox.ingress = {
+      "prometheus.albinvass.se" = "http://localhost:${builtins.toString config.services.prometheus.port}";
+      "elastic.albinvass.se" = "http://localhost:${builtins.toString config.services.elasticsearch.port}";
+      "grafana.albinvass.se" = "http://localhost:${builtins.toString config.services.grafana.settings.server.http_port}";
+      "kibana.albinvass.se" = "http://localhost:${builtins.toString config.services.kibana.port}";
+    };
+    prometheus = {
+      enable = true;
+      stateDir = "prometheus";
+    };
+    elasticsearch = {
+      enable = true;
+    };
+    kibana = {
+      enable = true;
+    };
+    grafana = {
+      enable = true;
+      settings = {
+        server = {
+          root_url = "https://grafana.albinvass.se";
+        };
       };
     };
   };

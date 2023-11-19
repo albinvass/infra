0c034b82f10960dd249101be99e732f9be2d96d7
Albin Vass <albin.vass@gmail.com>
2023-11-19 18:43:45 +0100

    Add sites index

--- nixos/hosts/devbox/static-web-server/default.nix
+++ nixos/hosts/devbox/static-web-server/default.nix
@@ -1,9 +1,21 @@
-{ lib, ... }: {
+{ lib, config, ... }:
+{
   services.cloudflared.tunnels.devbox.ingress = {
     "albinvass.se" = "http://localhost:8787";
   };
   environment.etc."/static-web-server/index.html" = {
-    text = ''Welcome!'';
+    text = let
+      sites = builtins.attrNames config.services.cloudflared.devbox.ingress;
+      links = builtins.map (f: "<li><a href ${f}>${f}</a></li>") sites;
+      body = builtins.concatStringsSep "\n" links;
+    in ''
+    <html>
+    <div> Welcome! </div>
+    <ul>
+    ${links}
+    </ul>
+    <html>
+    '';
     user = "static-web-server";
     group = "static-web-server";
   };

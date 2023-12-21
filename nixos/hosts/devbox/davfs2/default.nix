0a83b958a37c513719134859734c3e7bf0e60774
Albin Vass <albinvass@gmail.com>
2023-12-21 17:26:27 +0100

    Migrate davfs2 to sops

--- nixos/hosts/devbox/davfs2/default.nix
+++ nixos/hosts/devbox/davfs2/default.nix
@@ -2,23 +2,21 @@
   environment.systemPackages = with pkgs; [
     davfs2
   ];
-  deployment = {
-    keys = {
-      "davfs2-secret" = {
-        name = "secrets";
-        destDir = "/etc/davfs2";
-        keyCommand = ["bws-get" "davfs2"];
-        user = "root";
-        group = "root";
-        permissions = "0600";
-      };
+  sops.secrets = {
+    "davfs2/secrets" = {
+      owner = "root";
+      group = "root";
+      mode = "0600";
+      path = "/etc/davfs2/secrets";
     };
   };
+
   users.groups.davfs2 = {};
   users.users.davfs2 = {
     group = "davfs2";
     isSystemUser = true;
   };
+
   fileSystems."/var/lib/davfs2" = {
       device = "https://u343362.your-storagebox.de";
       fsType = "davfs";

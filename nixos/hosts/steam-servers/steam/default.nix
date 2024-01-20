e6a5620db53b5157659586c50ec41c863c236118
Albin Vass <albinvass@gmail.com>
2024-01-20 16:59:15 +0000

    Add palworld server

--- nixos/hosts/steam-servers/steam/default.nix
+++ nixos/hosts/steam-servers/steam/default.nix
@@ -1,5 +1,7 @@
-{...}: {
+{ steam-fetcher, ... }: {
   imports = [
     ./valheim.nix
+    ./palworld.nix
  ];
+  nixpkgs.overlays = [ steam-fetcher.overlays.default ];
 }
--- nixos/hosts/steam-servers/steam/palworld.nix
+++ nixos/hosts/steam-servers/steam/palworld.nix
@@ -1,34 +1,33 @@
-{config, pkgs, lib, ...}:
+{ config, pkgs, lib, ... }:
 {
-  users.users.valheim = {
+  users.groups.palworld = {};
+  users.users.palworld = {
     isSystemUser = true;
-    # Valheim puts save data in the home directory.
-    home = "/var/lib/valheim";
+    home = "/var/lib/palworld";
     createHome = true;
     homeMode = "750";
-    group = "valheim";
+    group = "palworld";
   };
 
-  users.groups.valheim = {};
 
   sops.secrets = {
-    "valheim-server/EnvironmentFile" = {
-      owner = "valheim";
-      group = "valheim";
+    "palworld-server/EnvironmentFile" = {
+      owner = "palworld";
+      group = "palworld";
       mode = "0600";
-      restartUnits = [ "valheim.service" ];
+      restartUnits = [ "palworld.service" ];
     };
   };
 
-  systemd.services.valheim = let
-    valheim-server = pkgs.stdenv.mkDerivation rec {
-      name = "valheim";
+  systemd.services.palworld = let
+    palworld-server = pkgs.stdenv.mkDerivation rec {
+      name = "palworld";
       src = pkgs.fetchSteam {
         inherit name;
-        appId = "896660";
-        depotId = "896661";
-        manifestId = "252049227837324070";
-        hash = "sha256-FZNhYn8SCuwALz5LMeOYuU2Hsc6q/pTyovqCG2wdOKs=";
+        appId = "2394010";
+        depotId = "2394012";
+        manifestId = "4603741190199642564";
+        hash = "sha256-NEnskCOl031yb0+jmsWkFHMZVVrRzM4BLLVZGko1Jk8=";
       };
 
       # Skip phases that don't apply to prebuilt binaries.
@@ -36,29 +35,26 @@
       dontConfigure = true;
       dontFixup = true;
 
-      buildInputs = with pkgs; [
-        libz
-      ];
+      buildInputs = with pkgs; [];
       nativeBuildInputs = with pkgs; [
         autoPatchelfHook
       ];
       installPhase = ''
         runHook preInstall
 
+        ls -al
         mkdir -p $out
         cp -r ./* $out/
-        chmod +x $out/valheim_server.x86_64
-        patchelf --set-interpreter ${pkgs.glibc}/lib/ld-linux-x86-64.so.2 $out/valheim_server.x86_64
-        patchelf --add-needed ${pkgs.libz}/lib/libz.so $out/valheim_server_Data/MonoBleedingEdge/x86_64/libmonobdwgc-2.0.so
-        patchelf --add-needed ${pkgs.libz}/lib/libz.so $out/valheim_server_Data/MonoBleedingEdge/x86_64/libMonoPosixHelper.so
+        #chmod +x $out/valheim_server.x86_64
+        #patchelf --set-interpreter ${pkgs.glibc}/lib/ld-linux-x86-64.so.2 $out/valheim_server.x86_64
 
         runHook postInstall
       '';
 
       meta = with lib; {
-        description = "Valheim dedicated server";
-        homepage = "https://steamdb.info/app/896660/";
-        changelog = "https://store.steampowered.com/news/app/896660?updates=true";
+        description = "Palworld dedicated server";
+        homepage = "https://steamdb.info/app/2394010/";
+        changelog = "https://store.steampowered.com/news/app/2394010?updates=true";
         sourceProvenance = with sourceTypes; [
           binaryNativeCode # Steam games are always going to contain some native binary component.
           binaryBytecode # e.g. Unity games using C#
@@ -70,9 +66,9 @@
   in {
     wantedBy = [ "multi-user.target" ];
     serviceConfig = let
-        script = pkgs.writeScriptBin "valheim-server" /* bash */ ''
+        script = pkgs.writeScriptBin "palworld-server" /* bash */ ''
           #!${pkgs.bash}/bin/bash
-          ${valheim-server}/valheim_server.x86_64 \
+          ${palworld-server}/valheim_server.x86_64 \
             -nographics \
             -batchmode \
             -savedir /var/lib/valheim/save \
@@ -85,14 +81,14 @@
         '';
       in {
       ExecStart = [
-        "${script}/bin/valheim-server"
+        "${script}/bin/palworld-server"
       ];
       Nice = "-5";
       PrivateTmp = true;
       Restart = "always";
-      User = "valheim";
+      User = "palworld";
       WorkingDirectory = "~";
-      EnvironmentFile = config.sops.secrets."valheim-server/EnvironmentFile".path;
+      EnvironmentFile = config.sops.secrets."palworld-server/EnvironmentFile".path;
     };
     environment = {
       LD_LIBRARY_PATH = "${pkgs.steamworks-sdk-redist}/lib";
@@ -100,6 +96,6 @@
     };
   };
   networking.firewall = {
-    allowedUDPPorts = [ 2456 2457 ];
+    allowedUDPPorts = [ 8211 ];
   };
 }

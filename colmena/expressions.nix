1b948c8757d1f6c902ca44dbf701cbd43effec8b
Albin Vass <albinvass@gmail.com>
2024-05-01 21:35:42 +0200

    Migrate pulumi to go

--- colmena/expressions.nix
+++ colmena/expressions.nix
@@ -1,13 +1,47 @@
-{ nodes, ... }:
+{ nodes, lib, ... }:
 rec {
-  enabledNodeConfigs = (
-    builtins.filter
-      (node: node.enabled)
+  nodeConfigs =
       (map
         (node: with (nodes.${node}.config); {
           name=node; deployment=deployment; enabled=builtins.elem "enabled" deployment.tags;
         })
-        (builtins.attrNames nodes))
+        (builtins.attrNames nodes));
+  cloudNode =
+    (node:
+      builtins.elem
+        "pulumi"
+        (builtins.map
+          (tag:
+            (if lib.strings.hasPrefix
+              "pulumi:"
+              tag
+             then "pulumi"
+             else tag))
+          node.deployment.tags));
+  getPulumiConfig =
+    (node:
+      if cloudNode node
+      then
+        builtins.fromJSON
+        (lib.strings.removePrefix
+          "pulumi:"
+          (builtins.elemAt
+            (builtins.filter
+              (tag: lib.strings.hasPrefix "pulumi:" tag)
+              node.deployment.tags)
+            0))
+      else {Server={Enabled=false;};});
+  pulumiConfigs =
+    builtins.map
+      (node: {
+          Name=node.deployment.targetHost;
+          Tunnels=nodes.${node.name}.config.services.cloudflared.tunnels;
+        } // getPulumiConfig node)
+      nodeConfigs;
+  enabledNodeConfigs = (
+    builtins.filter
+      (node: node.enabled)
+      nodeConfigs
   );
   enabledNodes = map (node: node.name) enabledNodeConfigs;
 }

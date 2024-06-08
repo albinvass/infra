57f0f86d1f7a378a20df48067c6f8e06e9cfb94b
Albin Vass <albinvass@gmail.com>
2024-06-08 17:09:25 +0200

    nix fmt

--- nixos/hosts/reverse-proxy/nginx.nix
+++ nixos/hosts/reverse-proxy/nginx.nix
@@ -11,23 +11,34 @@
   };
   services.nginx = {
     enable = true;
-    virtualHosts = let
-      hostDefaults = {
-        forceSSL = true;
-        enableACME = true;
-      };
-      frpProxies = lib.attrsets.mergeAttrsList (map (node: with nodes.${node}.config.services.frp;
-        if builtins.hasAttr "proxies" settings
-        then builtins.listToAttrs(map (proxy: { name = "${proxy.name}"; value = hostDefaults // {
-          locations = {
-            "/" = {
-              proxyPass = "http://127.0.0.1:${builtins.toString proxy.remotePort}";
-            };
-          };
-        };}) settings.proxies)
-        else {}
-      ) (builtins.attrNames nodes));
-
-    in frpProxies;
+    virtualHosts =
+      let
+        hostDefaults = {
+          forceSSL = true;
+          enableACME = true;
+        };
+        frpProxies = lib.attrsets.mergeAttrsList (
+          map (
+            node:
+            with nodes.${node}.config.services.frp;
+            if builtins.hasAttr "proxies" settings then
+              builtins.listToAttrs (
+                map (proxy: {
+                  name = "${proxy.name}";
+                  value = hostDefaults // {
+                    locations = {
+                      "/" = {
+                        proxyPass = "http://127.0.0.1:${builtins.toString proxy.remotePort}";
+                      };
+                    };
+                  };
+                }) settings.proxies
+              )
+            else
+              { }
+          ) (builtins.attrNames nodes)
+        );
+      in
+      frpProxies;
   };
 }

675d865f6b7167d2812f5ba0e298cf01bb593878
Albin Vass <albinvass@gmail.com>
2024-07-21 11:45:26 +0200

    Add persistent storage to atticd

--- nixos/hosts/nixpi/attic.nix
+++ nixos/hosts/nixpi/attic.nix
@@ -1,5 +1,7 @@
-{ config, inputs, ... }:
-{
+{ config, lib, inputs, ... }:
+let
+  atticUser = "atticd";
+in {
   imports = [ inputs.attic.nixosModules.atticd ];
   sops.secrets = {
     "attic/credentialsFile" = {
@@ -8,11 +10,26 @@
     };
   };
 
+  users = {
+    users.${atticUser} = {
+      group = atticUser;
+      isSystemUser = true;
+    };
+    groups.${atticUser} = {};
+  };
+
+  systemd.services.atticd.serviceConfig = {
+    StateDirectory = lib.mkForce null;
+    ReadWritePaths = lib.mkForce [ (builtins.dirOf config.services.atticd.settings.storage.path) ];
+  };
   services.atticd = {
     enable = true;
+    user = atticUser;
     credentialsFile = config.sops.secrets."attic/credentialsFile".path;
     settings = {
       listen = "127.0.0.1:8080";
+      database = {};
+      api-endpoint = "https://attic.albinvass.se/";
       chunking = {
         nar-size-threshold = 64 * 1024; # 64 KiB
         min-size = 16 * 1024; # 16 KiB
@@ -20,5 +37,9 @@
         max-size = 256 * 1024; # 256 KiB
       };
     };
+  };
+  fileSystems."${builtins.dirOf config.services.atticd.settings.storage.path}" = {
+    device = "storage:/volume1/attic";
+    fsType = "nfs";
   };
 }

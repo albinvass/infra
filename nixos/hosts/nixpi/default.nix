1819c22dc0576f3c60bb56ddea682df99fcbaf3c
Albin Vass <albinvass@gmail.com>
2025-03-07 18:24:53 +0100

    Fix poe hat for raspberries

--- nixos/hosts/nixpi/default.nix
+++ nixos/hosts/nixpi/default.nix
@@ -13,6 +13,8 @@
     inputs.nixos-hardware.nixosModules.raspberry-pi-4
   ];
 
+  hardware.raspberry-pi."4".poe-plus-hat.enable = true;
+
   sops = {
     age.sshKeyPaths = [ "/etc/ssh/ssh_host_ed25519_key" ];
     defaultSopsFile = ./secrets.yaml;
